d93a183f17f998a584ea872cb0f8b8c71ea1460b
VentVert <shoichi0512ichikawa@gmail.com>
2024-01-01 14:42:37 +0900

    already commited

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+# test
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
